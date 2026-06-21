bc7906a3f3dc1dcdb77f33003a3d2d615576beb1
Pallavi Rajput <rajput15pallavi@gmail.com>
2026-06-21 12:49:31 +0530

    day2

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const testRoutes = require("./src/routes/test.routes");
+const connectDB = require("./src/config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Student City Relocation API Running");
+});
+app.use("/api/test", testRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
+});
